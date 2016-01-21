94ea8c9632927d239ded5a951a7afce5ad0f06f0
iashin <iashin.d@nixsolutions.com>
2016-01-21 17:31:07 +0200

    bowerrc

--- public/app/lazyLoadConfig.js
+++ public/app/lazyLoadConfig.js
@@ -8,16 +8,6 @@
       events: true,
       modules: [
         {
-          name: 'nvd3',
-          files: [
-            'bower_components/d3/d3.min.js',
-            'bower_components/nvd3/build/nv.d3.js',
-            'bower_components/angular-nvd3/dist/angular-nvd3.js',
-            'bower_components/nvd3/build/nv.d3.min.css'
-          ],
-          serie: true
-        },
-        {
           name: 'datepicker',
           files: [
             'bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css',
