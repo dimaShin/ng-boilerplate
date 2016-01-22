dd2c0092c8f2a92bce93cfe240a8801c0bc52f22
iashin <iashin.d@nixsolutions.com>
2016-01-22 18:00:41 +0200

    http-server

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,22 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const jade = require('gulp-jade');
 const watch = require('gulp-watch');
+const argv = require('yargs').argv;
 
 const jsTask = require('./gulp-tasks/js');
 const cssTask = require('./gulp-tasks/css');
 
 gulp.task('init', () => {
-  gulp.start(['jade']);
-  jsTask({
-    env: 'dev'
-  });
 
-  cssTask({
-    env: 'dev'
-  });
+  const options = {
+    env: argv.env
+  };
+
+  gulp.start(['jade']);
+
+  jsTask(options);
+
+  cssTask(options);
 });
 
 gulp.task('jade', () => {
